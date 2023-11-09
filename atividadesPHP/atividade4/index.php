36e8c9ae219590df8225c592075a8cd0e629ac9f
Nicolas Klaumann <nicolas@conectra.com.br>
2023-11-09 18:51:27 -0300

    fix: ajusta atividade 4 e adiciona atividade 5

--- atividadesPHP/atividade4/index.php
+++ atividadesPHP/atividade4/index.php
@@ -4,13 +4,13 @@
     <title>Calculadora de Soma</title>
 </head>
 <body>
-    <h1>tamanho de um dos lados a da base do triangulo:</h1>
+    <h1>tamanho da altura e largura do retangulo:</h1>
     <form method="post">
-        <label for="valorBase">Valor base:</label>
-        <input type="number" id="valorBase" name="valorBase" required><br><br>
+        <label for="altura">Altura:</label>
+        <input type="number" id="altura" name="altura" required><br><br>
 
-        <label for="valorLado">Valor lado:</label>
-        <input type="number" id="valorLado" name="valorLado" required><br><br>
+        <label for="largura">Largura:</label>
+        <input type="number" id="largura" name="largura" required><br><br>
 
         <input type="submit" value="Calcular">
     </form>
@@ -18,11 +18,16 @@
     <?php
     // verifica se o formulário foi enviado
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $valorLado = $_POST['valorLado'];
-        $valorBase = $_POST['valorBase'];
+        $largura = $_POST['largura'];
+        $altura = $_POST['altura'];
 
-        $resultado = ($valorBase * $valorLado) / 2;
-        echo "<p>A area do triangulo é: $resultado</p>";
+        $resultado = $altura * $largura;
+
+        if ($resultado > 10) {
+            echo "<h1>A area do retangulo é: $resultado</h1>";
+        } else {
+            echo "<h3>A area do retangulo é: $resultado</h3>";
+        }
     }
     ?>
 </body>
---
+++ atividadesPHP/atividade5/README.md
@@ -0,0 +1,1 @@
+Crie um programa que calcule a área de um triângulo retângulo, cuja fórmula segue abaixo. Crie as variáveis apropriadas para o cálculo em PHP e por fim exiba uma frase com o valor da operação.
